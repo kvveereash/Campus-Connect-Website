d05bf2425a481de33cfb41dc55123eaacb133f91
Veereaswaran K.V <veereashkv@gmail.com>
2026-01-27 19:04:34 +0530

    fix: resolve chat import errors and cleanup chat page

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -1,12 +1,7 @@
 import ChatLayout from '@/components/chat/ChatLayout';
-import RoomList from '@/components/chat/RoomList';
-import ChatWindow from '@/components/chat/ChatWindow';
 
 export default function ChatPage() {
     return (
-        <ChatLayout>
-            <RoomList />
-            <ChatWindow />
-        </ChatLayout>
+        <ChatLayout />
     );
 }
